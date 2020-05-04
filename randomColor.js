1e4f3599e714157f28066a7d7c36111b0e1b2e89
plebtech <nbmc@protonmail.com>
2020-05-04 16:02:16 -0500

    structured test files.

--- randomColor.js
+++ randomColor.js
@@ -1,6 +1,8 @@
 const rgb = (r, g, b) => {
-    let color;
-
+    let r = Math.floor(Math.random() * 256);
+    let g = Math.floor(Math.random() * 256);
+    let b = Math.floor(Math.random() * 256);
+    const color = "rgb(" + r + "," + g + "," + b + ")";
     return color;
 }
 
---
+++ test.js
@@ -0,0 +1,2 @@
+import color as './randomColor.js';
+
