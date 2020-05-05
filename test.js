ddbfcf22bb0289e02dc054ad43217504bd45c763
plebtech <nbmc@protonmail.com>
2020-05-05 14:30:35 -0500

    fiddled with test page.

--- test.js
+++ test.js
@@ -2,16 +2,17 @@ document.addEventListener('DOMContentLoaded', onLoad());
 
 // script to run on load (called by DOMContentLoaded event above).
 function onLoad() {
-    // call bgColorChange every second.
+    // call bgColorChange every five seconds.
     document.body.style.display = 'flex';
     document.body.style.flexFlow = 'row wrap';
     document.body.style.justifyContent = 'space-around';
     const bgTimer = setInterval(() => {
         bgColorChange();
-    }, 1000);
-    // change textChange every second.
+    }, 5000);
+    // call hexChange and rgbChange every two seconds.
     const textTimer = setInterval(() => {
-        textChange();
+        hexChange();
+        rgbChange();
     }, 2000);
 }
 
@@ -20,8 +21,18 @@ const bgColorChange = () => {
     document.body.style.backgroundColor = rgb();
 }
 
-const textChange = () => {
+const rgbChange = () => {
     const color = rgb();
+    const hOne = document.createElement('h1');
+    const hOneText = document.createTextNode(color);
+    hOne.appendChild(hOneText);
+    hOne.style.margin = '1%';
+    hOne.style.color = color;
+    document.body.prepend(hOne);
+}
+
+const hexChange = () => {
+    const color = hex();
     const hOne = document.createElement('h1');
     const hOneText = document.createTextNode(color);
     hOne.appendChild(hOneText);
