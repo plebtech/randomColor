189afbd780a6e08afccca1f788dfa6907b11cf77
plebtech <nbmc@protonmail.com>
2020-05-05 14:26:15 -0500

    implemented hex function.

--- randomColor.js
+++ randomColor.js
@@ -2,10 +2,23 @@ const rgb = () => {
     let r = Math.floor(Math.random() * 256);
     let g = Math.floor(Math.random() * 256);
     let b = Math.floor(Math.random() * 256);
-    const color = "rgb(" + r + "," + g + "," + b + ")";
+    const color = `rgb(${r},${g},${b})`;
     return color;
 }
 
 const hex = () => {
-    
+    const r = addZero(Math.floor(Math.random() * 256).toString(16));
+    const g = addZero(Math.floor(Math.random() * 256).toString(16));
+    const b = addZero(Math.floor(Math.random() * 256).toString(16));
+    const color = `#${r}${g}${b}`;
+    return color;
+}
+
+const addZero = (x) => {
+    if (x.length === 1) {
+        const y = '0' + x;
+        return y;
+    } else {
+        return x;
+    }
 }
--- test.js
+++ test.js
@@ -1,11 +1,31 @@
-document.addEventListener("DOMContentLoaded", function () {
-    // timer to change background color every second.
-    const timer = setInterval(() => {
+document.addEventListener('DOMContentLoaded', onLoad());
+
+// script to run on load (called by DOMContentLoaded event above).
+function onLoad() {
+    // call bgColorChange every second.
+    document.body.style.display = 'flex';
+    document.body.style.flexFlow = 'row wrap';
+    document.body.style.justifyContent = 'space-around';
+    const bgTimer = setInterval(() => {
         bgColorChange();
     }, 1000);
-});
+    // change textChange every second.
+    const textTimer = setInterval(() => {
+        textChange();
+    }, 2000);
+}
 
 // calls randomColor.rbg() to generate a random color.
 const bgColorChange = () => {
     document.body.style.backgroundColor = rgb();
+}
+
+const textChange = () => {
+    const color = rgb();
+    const hOne = document.createElement('h1');
+    const hOneText = document.createTextNode(color);
+    hOne.appendChild(hOneText);
+    hOne.style.margin = '1%';
+    hOne.style.color = color;
+    document.body.prepend(hOne);
 }
